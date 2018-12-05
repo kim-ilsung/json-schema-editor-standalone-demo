2d3ab14ffd957a2ec9be3d062c511491c5e8c968
root <root@localhost>
2018-12-05 15:57:58 +0300

    Import

---
+++ static/js/vendors.b21c376433464c92c9c0.js
@@ -0,0 +1,19 @@
+webpackJsonp([2],{
+
+/***/ 213:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 306:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(72);
+
+
+/***/ })
+
+},[306]);
+//# sourceMappingURL=vendors.b21c376433464c92c9c0.js.map
