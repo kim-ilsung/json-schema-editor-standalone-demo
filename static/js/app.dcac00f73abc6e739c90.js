22c7ced82832835e43f356bb7a2706c635e65d71
root <root@localhost>
2018-12-05 17:49:01 +0300

    1

--- static/js/app.dcac00f73abc6e739c90.js
+++ static/js/app.dcac00f73abc6e739c90.js
@@ -2471,7 +2471,7 @@ var clipboard = {};
     this.repository = __WEBPACK_IMPORTED_MODULE_10__json_schema_editor__["repository"];
     this.setContainerHeight();
 
-    window.parent.test({'kk':123});
+    //window.parent.test({'kk':123});
 
     window.addEventListener('resize', function () {
       if (!_this.timeout) {
